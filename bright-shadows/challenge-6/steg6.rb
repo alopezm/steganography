2bfca67763e6cb451405f3ebef207618054e0851
Juan David Pineda C <juan.david.pineda@gmail.com>
2011-09-13 23:51:59 -0500

    param management added, but you can enhance it.

--- bright-shadows/challenge-6/steg6.rb
+++ bright-shadows/challenge-6/steg6.rb
@@ -1,26 +1,30 @@
 #Taken from http://www.imagemagick.org/RMagick/doc/comtasks.html
 require 'Rmagick'
-ARGV.each { |file|
-  puts file
-  img = Magick::Image::read(file).first
-  puts "   Format: #{img.format}"
-  puts "   Geometry: #{img.columns}x#{img.rows}"
-  puts "   Class: " + case img.class_type
-                      when Magick::DirectClass
-                        "DirectClass"
-                      when Magick::PseudoClass
-                        "PseudoClass"
-                      end
-  puts "   Depth: #{img.depth} bits-per-pixel"
-  puts "   Colors: #{img.number_colors}"
-  puts "   Filesize: #{img.filesize}"
-  puts "   Resolution: #{img.x_resolution.to_i}x#{img.y_resolution.to_i} "+
-  "pixels/#{img.units == Magick::PixelsPerInchResolution ?
+if ARGV.first.nil? #you can enhance this with param management.
+  puts "I need an image file"
+else
+  ARGV.each { |file|
+    puts file
+    img = Magick::Image::read(file).first
+    puts "   Format: #{img.format}"
+    puts "   Geometry: #{img.columns}x#{img.rows}"
+    puts "   Class: " + case img.class_type
+                        when Magick::DirectClass
+                          "DirectClass"
+                        when Magick::PseudoClass
+                          "PseudoClass"
+                        end
+    puts "   Depth: #{img.depth} bits-per-pixel"
+    puts "   Colors: #{img.number_colors}"
+    puts "   Filesize: #{img.filesize}"
+    puts "   Resolution: #{img.x_resolution.to_i}x#{img.y_resolution.to_i} "+
+    "pixels/#{img.units == Magick::PixelsPerInchResolution ?
         "inch" : "centimeter"}"
-  if img.properties.length > 0
-    puts "   Properties:"
-    img.properties { |name,value|
-      puts %Q|      #{name} = "#{value}"|
-    }
-  end
-}
+    if img.properties.length > 0
+      puts "   Properties:"
+      img.properties { |name,value|
+        puts %Q|      #{name} = "#{value}"|
+      }
+    end
+  }
+end
