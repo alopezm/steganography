2bd3a754d188a14eb71fc9e8d340d26e1c7439b9
Juan David Pineda C <juan.david.pineda@gmail.com>
2011-09-13 23:27:20 -0500

    added code to read metadata from a bmp file in ruby

---
+++ bright-shadows/challenge-6/steg6.rb
@@ -0,0 +1,26 @@
+#Taken from http://www.imagemagick.org/RMagick/doc/comtasks.html
+require 'Rmagick'
+ARGV.each { |file|
+  puts file
+  img = Magick::Image::read(file).first
+  puts "   Format: #{img.format}"
+  puts "   Geometry: #{img.columns}x#{img.rows}"
+  puts "   Class: " + case img.class_type
+                      when Magick::DirectClass
+                        "DirectClass"
+                      when Magick::PseudoClass
+                        "PseudoClass"
+                      end
+  puts "   Depth: #{img.depth} bits-per-pixel"
+  puts "   Colors: #{img.number_colors}"
+  puts "   Filesize: #{img.filesize}"
+  puts "   Resolution: #{img.x_resolution.to_i}x#{img.y_resolution.to_i} "+
+  "pixels/#{img.units == Magick::PixelsPerInchResolution ?
+        "inch" : "centimeter"}"
+  if img.properties.length > 0
+    puts "   Properties:"
+    img.properties { |name,value|
+      puts %Q|      #{name} = "#{value}"|
+    }
+  end
+}
